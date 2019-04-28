46086ed2ea6b0ee0517cf4a53156406ecd581517
Rohan Acharya <rohan.acharya@sjsu.edu>
2019-04-27 22:56:28 -0700

    Added Follow Question API

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -1,23 +1,25 @@
 var express = require("express");
 var question = express.Router();
 var QuestionModel = require("../model/QuestionSchema");
-var kafka = require('../kafka/client');
+var UserModel = require("../model/UserSchema");
+var kafka = require("../kafka/client");
+var client = require('../resources/redis');
 
+//Follow a Question
 
-question.post("/questions/:questionID/follow", async (req, res) => {
-
-    kafka.make_request('follow_question', req.body, function (err, results) {
-        console.log("In follow question - kafka make request")
-        if (err) {
-            console.log("Inside err");
-            res.json({
-                status: "error",
-                msg: "System Error, Try Again."
-            });
-            res.end();
-        } else {
-            console.log("Inside else");
-            res.status(200).json(results);
-        }
-    })
+question.post("/questions/follow", async (req, res) => {
+	try {
+		let { userId, questionId } = req.body;
+		let result = await QuestionModel.update(
+			{ _id: questionId },
+			{
+				$push: { followers: userId }
+			}
+		);
+		res.status(200).json({});
+	} catch (error) {
+		res.send(error);
+	}
 });
+
+module.exports = question;
