fc84c0b199b446af502f2ce00f0ab092d5a9bda5
Rohan Acharya <rohan.acharya@sjsu.edu>
2019-04-27 21:13:41 -0700

    Added config files

---
+++ backend/resources/mongosse.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+mongoose
+	.connect(`${process.env.DB_HOST}`, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		poolSize: 500
+	})
+	.then(
+		() => {
+			console.log(`Sucessfully Connected to MogoDB`);
+		},
+		err => {
+			console.log(`Error Connecting to MogoDB: ${err}`);
+		}
+	);
+
+module.exports = { mongoose };
