686e21432fa19cb8911713b94ba60a0cf1204512
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-04-27 22:46:46 -0700

    Get User by ID API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,50 @@ const mongoose = require('../resources/mongoose');
 const UserSchema = require('../model/UserSchema')
 
 
+router.get('/:userId', async (request, response) => {
+
+	console.log(`\n\nInside GET /users/:userId`);
+
+	// try {
+	// 	let userDocument = await UserSchema.findOneAndUpdate(
+	// 		{ _id: request.params.userId },
+	// 		{ $inc: { views : 1 } },
+	// 		{ new: true }
+	// 	);
+
+	// 	//If user present
+	// 	if(userDocument) {
+	// 		console.log(`Sucessfully fetched user ${request.params.userId}:\n ${userDocument}`);
+	// 		response.status(200).json(userDocument);
+	// 	} else {
+	// 		console.log(`User ${request.params.userId} not found`);
+	// 		response.status(404).json({messgage: `User ${request.params.userId} not found`});
+	// 	}
+	// } catch (error) {
+	// 	console.log(`Error fetching user ${request.params.userId}:\n ${error}`);
+	// 	response.status(500).json({ error: error, message: `Error fetching user ${request.params.userId}`});
+	// }
+
+	try {
+		let userDocument = await UserSchema.findOne(
+			{ _id: request.params.userId }
+		);
+
+		//If user present
+		if(userDocument) {
+			console.log(`Sucessfully fetched user ${request.params.userId}:\n ${userDocument}`);
+			response.status(200).json(userDocument);
+		} else {
+			console.log(`User ${request.params.userId} not found`);
+			response.status(404).json({messgage: `User ${request.params.userId} not found`});
+		}
+	} catch (error) {
+		console.log(`Error fetching user ${request.params.userId}:\n ${error}`);
+		response.status(500).json({ error: error, message: `Error fetching user ${request.params.userId}`});
+	}
+});
+
+
 router.get('/:userId/follow/enable', async (request, response) => {
 
 	console.log(`\n\nInside GET /users/:userId/follow/enable`);
