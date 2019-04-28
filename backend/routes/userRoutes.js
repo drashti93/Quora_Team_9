dc263b1d9712e186cd4b4e2ee882691c6f89dbba
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-04-27 21:59:06 -0700

    Async-Await - Fetch User followers API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -65,22 +65,27 @@ router.post('/:userId/follow/disable', async (request, response) => {
 });
 
 
-router.get('/:userId/followers', (request, response) => {
+router.post('/:userId/followers', async (request, response) => {
 
-	console.log(`\n\nInside GET /users/:userId/followers`);
+	console.log(`\n\nInside POST /users/:userId/followers`);
 
-	UserSchema.findOne(
-		{ userId: request.params.userId },
-		(error, userDocument) => {
-			if (error) {
-				console.log(`Error fetching followers for the user ${request.params.userId}:\n ${error}`);
-				response.status(500).json({ error: error, message: `Error fetching followers for the user ${request.params.userId}` });
-			} else {
-				console.log(`Sucessfully fetched followers for the user ${request.params.userId}:\n ${userDocument}`);
-				response.status(200).json(userDocument.followers);
-			}
+	try {
+		let userDocument = await UserSchema.findOne(
+			{ _id: request.params.userId }
+		);
+
+		//If user present
+		if(userDocument) {
+			console.log(`Sucessfully fetched followers for the user ${request.params.userId}:\n ${userDocument}`);
+			response.status(200).json(userDocument.followers);
+		} else {
+			console.log(`User ${request.params.userId} not found`);
+			response.status(404).json({messgage: `User ${request.params.userId} not found`});
 		}
-	);
+	} catch (error) {
+		console.log(`Error fetching followers for the user ${request.params.userId}:\n ${error}`);
+		response.status(500).json({ error: error, message: `Error fetching followers for the user ${request.params.userId}` });
+	}
 });
 
 
