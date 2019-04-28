bbafa69ca6a6a39bf15d383e1aa07dc19943c3a1
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-04-27 21:53:57 -0700

    Async-Await - Disable User follow API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,9 +5,9 @@ const mongoose = require('../resources/mongoose');
 const UserSchema = require('../model/UserSchema')
 
 
-router.post('/:userId/follow/enable',  (request, response) => {
+router.post('/:userId/follow/enable', async (request, response) => {
 
-	console.log(`\n\nInside GET /users/:userId/follow/enable`);
+	console.log(`\n\nInside POST /users/:userId/follow/enable`);
 
 	try {
 		let userDocument = await UserSchema.findOneAndUpdate(
@@ -35,28 +35,33 @@ router.post('/:userId/follow/enable',  (request, response) => {
 });
 
 
-router.get('/:userId/follow/disable', (request, response) => {
+router.post('/:userId/follow/disable', async (request, response) => {
 
-	console.log(`\n\nInside GET /users/:userId/follow/disable`);
+	console.log(`\n\nInside POST /users/:userId/follow/disable`);
 
-	UserSchema.findOneAndUpdate(
-		{ userId: request.params.userId },
-		{
-			$set: {
-				isFollowAllowed: false
-			}
-		},
-		{ new: true },
-		(error, userDocument) => {
-			if (error) {
-				console.log(`Error while disabling follow for the question ${request.params.userId}:\n ${error}`);
-				response.status(500).json({ error: error, message: `Error while enabling follow for the user ${request.params.userId}` });
-			} else {
-				console.log(`Sucessfully disabled follow for the question ${request.params.userId}:\n ${userDocument}`);
-				response.status(200).json(userDocument);
-			}
+	try {
+		let userDocument = await UserSchema.findOneAndUpdate(
+			{ _id: request.params.userId },
+			{
+				$set: {
+					isFollowAllowed: false
+				}
+			},
+			{ new: true }
+		);
+
+		//If user present
+		if(userDocument) {
+			console.log(`Sucessfully disabled follow for the question ${request.params.userId}:\n ${userDocument}`);
+			response.status(200).json(userDocument);
+		} else {
+			console.log(`User ${request.params.userId} not found`);
+			response.status(404).json({messgage: `User ${request.params.userId} not found`});
 		}
-	);
+	} catch (error) {
+		console.log(`Error while disabling follow for the question ${request.params.userId}:\n ${error}`);
+		response.status(500).json({ error: error, message: `Error while enabling follow for the user ${request.params.userId}` });
+	}
 });
 
 
