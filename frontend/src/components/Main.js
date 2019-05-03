529f230ab94c861c762e3d26a564c7aae8502b65
drashti93 <drustihitesh.thakkar@sjsu.edu>
2019-05-02 18:25:49 -0700

    Profile page boiler plate complete

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import cookie from 'react-cookies';
 import {Redirect} from 'react-router';
-<<<<<<< HEAD
 import Navigationbar from "./navbar/Navigationbar"
 import TopicBar from './topics/topic'
 
 // import '../App.css';
-=======
-import Navigationbar from "./navbar/Navigationbar";
+
 import Feed from "./feed/Feed";
->>>>>>> b50b1000882090eb736f882aeb5fa417fb1b49f0
 
 // Main Component
 class Main extends Component{
@@ -25,13 +22,10 @@ class Main extends Component{
             <Navigationbar/>
             <div className="container">
                 <div className="row">
-<<<<<<< HEAD
                     <div className="col-lg-2 col-md-2 col-xs-12"><TopicBar/></div>
                     <div className="col-lg-8 col-md-8 col-xs-12">Middle section</div>
-=======
                     <div className="col-lg-2 col-md-2 col-xs-12">Left side Bar</div>
                     <div className="col-lg-8 col-md-8 col-xs-12"><Feed/></div>
->>>>>>> b50b1000882090eb736f882aeb5fa417fb1b49f0
                     <div className="col-lg-2 col-md-2 col-xs-12">Right side bar</div>
                 </div>
                 </div>
