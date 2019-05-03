df46f60ff846e81eb1d61347d633378437841856
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-05-02 21:13:23 -0700

    Updating Main.js

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -24,8 +24,6 @@ class Main extends Component{
             <div className="container">
                 <div className="row">
                     <div className="col-lg-2 col-md-2 col-xs-12"><TopicBar/></div>
-                    <div className="col-lg-8 col-md-8 col-xs-12">Middle section</div>
-                    <div className="col-lg-2 col-md-2 col-xs-12">Left side Bar</div>
                     <div className="col-lg-8 col-md-8 col-xs-12">
                         <Route exact path="/" component={Feed} />
                         <Route path="/questions" component={Questions} />
