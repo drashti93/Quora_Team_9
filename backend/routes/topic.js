bd42a788be3f17cf2fbb739ca3b2ece6ba900245
acharyarohan <acharyarohan@users.noreply.github.com>
2019-05-03 17:02:31 -0700

    Update topic.js

--- backend/routes/topic.js
+++ backend/routes/topic.js
@@ -22,4 +22,37 @@ topic.post("/topics/follow", async (req, res) => {
 	}
 });
 
+//GET ALL QUESTIONS (FOR FEED)
+
+question.get("/following", (req, res) => {
+    let { _id } = req.body;
+
+    //let questions = await QuestionModel.find({}).populate({path:'chats.uid', select:["firstName"]})
+
+
+	QuestionModel.find({}, (err, result) => {
+		if (err) {
+			console.log("Error in retrieving questions", err);
+			res.writeHead(400, {
+				"Content-type": "text/plain"
+			});
+			res.end("Error in retrieving questions");
+		} else {
+			res.writeHead(200, {
+				"Content-type": "application/json"
+			});
+			let finalResult = [];
+			result.forEach(question => {
+				if (question.followers.includes(_id)) {
+					finalResult.push(question);
+				}
+			});
+
+			console.log("questions: ", finalResult);
+			res.end(JSON.stringify(finalResult));
+		}
+	});
+});
+
+
 module.exports = topic;
