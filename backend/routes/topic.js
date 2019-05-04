dd24213af81499affbf8e5874a6502c84e9da4ba
acharyarohan <acharyarohan@users.noreply.github.com>
2019-05-03 20:06:14 -0700

    Update topic.js

--- backend/routes/topic.js
+++ backend/routes/topic.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var topic = express.Router();
-var TopicModel = require("../model/TopicSchema");
+var QuestionModel = require("../model/QuestionSchema");
 var UserModel = require("../model/UserSchema");
 var kafka = require("../kafka/client");
-var client = require('../resources/redis');
+var client = require("../resources/redis");
 
 //Follow a Topic
 
@@ -22,43 +22,60 @@ topic.post("/topics/follow", async (req, res) => {
 	}
 });
 
-//GET QUESTIONS FOR A PARTICULAR TOPIC (FOR FEED)
-
-topic.get("/:topicId/questions/following", (req, res) => {
-    let { userId } = req.body;
-    let  topicId  = req.params.topicId;
-
-    UserModel.findOne({
-        _id : userId
-    }
-    , (err, result) => {
-        if (err) {
-			console.log("Error in retrieving questions", err);
-			res.writeHead(400, {
-				"Content-type": "text/plain"
+// Search topic
+topic.get("/:topicId", (request, response) => {
+	console.log(`\n\nInside Get /topics/:topicId`);
+	TopicModel.find({}, (error, topicDocument) => {
+		if (error) {
+			console.log(
+				`Error while getting topics ${
+					request.params.userId
+				}:\n ${error}`
+			);
+			response.status(500).json({
+				error: error,
+				message: `Error while getting topics ${request.params.userId}`
 			});
-			res.end("User not following particular topic!");
+		} else if (topicDocument) {
+			console.log(
+				`Error while getting topics ${
+					request.params.userId
+				}:\n ${topicDocument}`
+			);
+			let result = topicDocument;
+			response.status(200).json(result);
 		} else {
-
-            TopicModel.findOne({'_id' : topicId}, (err, result) => {
-                if (err) {
-                    console.log("error!", err); 
-                    res.writeHead(400, {
-                        "Content-type": "text/plain"
-                    });
-                    res.end("Error inside topic!");
-		} else {
-
-            console.log("questions: ", result.questions);
-			res.end(JSON.stringify(result.questions));
-
-            }
-        });
-        }
-    });
+			response.status(404).json({ message: `Topic not found` });
+		}
+	});
 });
 
+//GET QUESTIONS FOR A PARTICULAR TOPIC (FOR FEED)
 
+topic.get("/:topicId/questions/following", async (req, res) => {
+	let topicId = req.params.topicId;
 
+	let result = await QuestionModel.find({ topics: topicId })
+		.populate("answers")
+		.exec();
 
+	console.log("questions: ", result);
+	res.end(JSON.stringify(result));
+});
+
+//GET ALL QUESTIONS FOR ALL TOPICS FOLLOWED (FOR FEED)
+
+topic.get("/questions/following", async (req, res) => {
+	let result = await UserModel.find({}, "topicsFollowed")
+		.populate({
+			path: "questions"
+		})
+		.populate({
+			path: "answers"
+		})
+		.exec();
+
+	console.log("questions: ", result);
+	res.end(JSON.stringify(result));
+});
 module.exports = topic;
