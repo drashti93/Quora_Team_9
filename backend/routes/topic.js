741f9883336445faaf93e1971c460488e4316d65
Rohan Acharya <rohan.acharya@sjsu.edu>
2019-05-03 17:00:36 -0700

    Added Get Questions APIs

--- backend/routes/topic.js
+++ backend/routes/topic.js
@@ -57,4 +57,42 @@ topic.get("/:topicId", (request, response) => {
     )
 });
 
+//GET QUESTIONS FOR A PARTICULAR TOPIC (FOR FEED)
+
+topic.get("/:topicId/questions/following", (req, res) => {
+    let { userId } = req.body;
+    let  topicId  = req.params.topicId;
+
+    UserModel.findOne({
+        _id : userId
+    }
+    , (err, result) => {
+        if (err) {
+			console.log("Error in retrieving questions", err);
+			res.writeHead(400, {
+				"Content-type": "text/plain"
+			});
+			res.end("User not following particular topic!");
+		} else {
+
+            TopicModel.findOne({'_id' : topicId}, (err, result) => {
+                if (err) {
+                    console.log("error!", err); 
+                    res.writeHead(400, {
+                        "Content-type": "text/plain"
+                    });
+                    res.end("Error inside topic!");
+		} else {
+
+            console.log("questions: ", result.questions);
+			res.end(JSON.stringify(result.questions));
+
+            }
+        });
+        }
+    });
+});
+
+
+
 module.exports = topic;
