c149b279bc989a95f150b0618c4f7fca12d385a0
acharyarohan <acharyarohan@users.noreply.github.com>
2019-05-03 17:03:48 -0700

    Update topic.js

--- backend/routes/topic.js
+++ backend/routes/topic.js
@@ -22,6 +22,42 @@ topic.post("/topics/follow", async (req, res) => {
 	}
 });
 
+//GET QUESTIONS FOR A PARTICULAR TOPIC (FOR FEED)
+
+topic.get("/:topicId/questions/following", (req, res) => {
+    let { userId } = req.body;
+    let  topicId  = req.params.topicId;
+
+    UserModel.findOne({
+        _id : userId
+    }
+    , (err, result) => {
+        if (err) {
+			console.log("Error in retrieving questions", err);
+			res.writeHead(400, {
+				"Content-type": "text/plain"
+			});
+			res.end("User not following particular topic!");
+		} else {
+
+            TopicModel.findOne({'_id' : topicId}, (err, result) => {
+                if (err) {
+                    console.log("error!", err); 
+                    res.writeHead(400, {
+                        "Content-type": "text/plain"
+                    });
+                    res.end("Error inside topic!");
+		} else {
+
+            console.log("questions: ", result.questions);
+			res.end(JSON.stringify(result.questions));
+
+            }
+        });
+        }
+    });
+});
+
 
 
 
